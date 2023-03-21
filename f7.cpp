1c2aa1dfd15279b2c2a293d3d645f2fe77d3529c
eonesty <eonesty@naver.com>
2023-03-20 23:25:51 -0400

    Initial commit

---
+++ f7.cpp
@@ -0,0 +1,21 @@
+/*
+* Filename: f7.cpp
+* Project: Focused Assignment 7
+* By: Jongeon Lee
+* Date: Mar 24, 2023
+* Description: Write a program to give you practice at creating text and binary files.
+*/
+
+#include <stdio.h>
+#pragma warning(disable: 4996)
+
+int main()
+{
+
+
+
+
+
+
+	return 0;
+}
