70b6513663a6c985ff2a3d607704c7d8cd098e74
eonesty <eonesty@naver.com>
2023-03-20 23:53:38 -0400

    Add function for making txt file

--- f7.cpp
+++ f7.cpp
@@ -11,7 +11,31 @@
 
 int main()
 {
+    FILE* fp = NULL;
+    char record[81] = { 0 };
 
+    fp = fopen("myTextFile.txt", "w");
+
+    if (fp == NULL)
+    {
+        printf("Error-fopen function: Can't open file for writing\n");
+        return -1;
+    }
+
+    for (int i = 1; i < 3; i++)
+    {
+        if (fprintf(fp, "This is line %d.\n", i) < 0)
+        {
+            printf("Error-fopen function: Can't write line to file\n");
+            return -1;
+        }
+    }
+
+    if (fclose(fp) != 0)
+    {
+        printf("Error-fclose function:Can't close file opened for writing\n");
+        return -1;
+    }
 
 
 
